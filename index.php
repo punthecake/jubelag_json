436e1d23d99128318cbfddcd9d2b21ed0c647890
punthecake <60122576+punthecake@users.noreply.github.com>
2020-02-07 22:07:29 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$json = file_get_contents("https://jubelagjson.herokuapp.com/json.php");
+
+$data = json_decode($json,true);
+$list = $data['ballpeb'];
+
+
+?>
+
+<h1>Ballpens </h1>
+
+<table border="1px">
+    <tr>
+        <td>Name</td>
+        <td>Price</td>
+        <td>Color</td>
+        <td>Supplier</td>
+    </tr>
+<?php
+foreach($list as $value){
+    ?>
+    <tr>
+        <td><?php echo $value['name'];?></td>
+        <td><?php echo $value['price'];?></td>
+        <td><?php echo $value['color'];?></td>
+        <td><?php echo $value['supplier'];?></td>
+    </tr>
+<?php
+}
+?>
+</table>
