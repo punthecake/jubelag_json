47212cfa5685b6c94cbc4a1cc4d12d96f5fefe51
punthecake <60122576+punthecake@users.noreply.github.com>
2020-02-07 22:03:23 +0800

    Update json.php

--- json.php
+++ json.php
@@ -2,7 +2,7 @@
 include 'config.php';
 include 'class.ballpen.php';
 header('Content-Type: application/json');
-$ballpen = new ballpen();
+$ballpen = new Ballpen();
 $list=$ballpen->get_ballpen();
 echo "{\"ballpen\":";
 echo json_encode($list);
